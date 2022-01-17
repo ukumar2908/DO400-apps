2c94f38bccc002939efe72118e861d16b8fcfaee
ukumar2908 <ukumar2908@gmail.com>
2022-01-17 05:40:28 +0000

    Enhanced greeting

--- person.py
+++ person.py
@@ -1,5 +1,6 @@
 class Person:
- def greet(self) -> None:
- print('Hello Red Hatter!')
+ def greet(self, name: str = 'Red Hatter') -> None:
+ print('Hello {}!'.format(name))
 pablo = Person()
-pablo.greet()
+pablo.greet('Jaime')
+pablo.greet('Guy')
